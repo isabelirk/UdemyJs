a4b5eef57677e866856807f09b270ffa0041d02d
Isabeli Reik <isabelireik2@gmail.com>
2022-03-02 13:35:15 -0300

    par/ímpar

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert("Ola mundo!");
+//alert("Olá mundo!");
+
+/*
+comentário também
+*/
+
+var mensagem = "Olá mundo!";
+alert(mensagem);
+
+var a = 2;
+var b = 3;
+var soma = a + b;
+var sub = a - b;
+var mult = a * b;
+var div = a / b;
+
+//alert(soma);
+//alert(sub);
+//alert(mult);
+//alert(div);
+
+var mod = a % b;
+//alert(mod);
+
+if(b % 2 == 1){
+    alert("Número ímpar!");
+}else if (b % 2 == 0){
+    alert("Número par!");
+}else{
+    alert("Valor inválido!");
+}
